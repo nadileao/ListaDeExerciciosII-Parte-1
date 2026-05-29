2632d36bbf02efe7d5ae393e5613ff4a740d4c89
Nádia Leão <nadialeaoxavier@gmail.com>
2026-05-29 10:41:33 -0400

    Add Podcast class that inherits from Midia

--- SistemaEducacional/Models/podcast.py
+++ SistemaEducacional/Models/podcast.py
@@ -1,1 +1,9 @@
+from models.midia import Midia
 
+class Podcast(Midia):
+    def __init__(self, titulo, duracao, apresentador):
+        super().__init__(titulo, duracao)
+        self.apresentador = apresentador
+
+    def reproduzir(self):
+        print(f"Reproduzindo podcast '{self.titulo}' com {self.apresentador}")
