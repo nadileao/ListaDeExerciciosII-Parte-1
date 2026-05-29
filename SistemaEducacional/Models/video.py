4c6338cb98af6674a3c4f9b7134940f4692c8427
Nádia Leão <nadialeaoxavier@gmail.com>
2026-05-29 10:40:53 -0400

    Update video.py

--- SistemaEducacional/Models/video.py
+++ SistemaEducacional/Models/video.py
@@ -1,1 +1,9 @@
+from models.midia import Midia
 
+class Video(Midia):
+    def __init__(self, titulo, duracao, resolucao):
+        super().__init__(titulo, duracao)
+        self.resolucao = resolucao
+
+    def reproduzir(self):
+        print(f"Reproduzindo vídeo '{self.titulo}' em {self.resolucao}")
