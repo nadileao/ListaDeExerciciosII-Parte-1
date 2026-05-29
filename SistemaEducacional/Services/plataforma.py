761ad46bf8a29fa69895d9cd8590bf46cee1df96
Nádia Leão <nadialeaoxavier@gmail.com>
2026-05-29 10:43:28 -0400

    Implement Plataforma class with media management methods

--- SistemaEducacional/Services/plataforma.py
+++ SistemaEducacional/Services/plataforma.py
@@ -1,1 +1,17 @@
+class Plataforma:
+    def __init__(self, nome):
+        self.nome = nome
+        self.midias = []
 
+    def adicionar_midia(self, midia):
+        self.midias.append(midia)
+
+    def listar_midias(self):
+        print(f"\n Mídias disponíveis em '{self.nome}':")
+        for midia in self.midias:
+            midia.mostrar_info()
+
+    def reproduzir_todas(self):
+        print(f"\n Reproduzindo todas as mídias de '{self.nome}':")
+        for midia in self.midias:
+            midia.reproduzir()
