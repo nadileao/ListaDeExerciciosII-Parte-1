44de6222ee0468c139d4addcc479db384cd967e3
Nádia Leão <nadialeaoxavier@gmail.com>
2026-05-29 10:42:27 -0400

    Update texto_narrado.py

--- SistemaEducacional/Models/texto_narrado.py
+++ SistemaEducacional/Models/texto_narrado.py
@@ -1,1 +1,9 @@
+from models.midia import Midia
 
+class TextoNarrado(Midia):
+    def __init__(self, titulo, duracao, idioma):
+        super().__init__(titulo, duracao)
+        self.idioma = idioma
+
+    def reproduzir(self):
+        print(f"Reproduzindo texto narrado '{self.titulo}' em {self.idioma}")
